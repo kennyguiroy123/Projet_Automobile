69885f4b5782c72ad6e1efab147a015ce2a66dbb
bacary <bacaryd33@gmail.com>
2021-01-12 22:46:50 +0100

    j'ai poussé client

--- src/main/java/auto/spring/dao/ClientDAO.java
+++ src/main/java/auto/spring/dao/ClientDAO.java
@@ -1,1 +1,30 @@
 package auto.spring.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import auto.spring.modele.Client;
+
+@Repository
+public class ClientDAO {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	public void sauver(Client client) {
+		em.persist(client);
+	}
+
+	public Client getclientById(Long idclient) {
+		return em.find(Client.class, idclient);
+	}
+
+	public List<Client> getclients() {
+		return em.createQuery("select * from client", Client.class).getResultList();
+	}
+
+}
