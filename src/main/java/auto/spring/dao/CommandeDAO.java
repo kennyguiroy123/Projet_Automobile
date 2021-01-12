47f337d2202cbb7f96a02f192b8fa708e0f79f47
bacary <bacaryd33@gmail.com>
2021-01-12 22:38:40 +0100

    la je finis commande et produit tout juste

--- src/main/java/auto/spring/dao/CommandeDAO.java
+++ src/main/java/auto/spring/dao/CommandeDAO.java
@@ -20,26 +20,12 @@ public class CommandeDAO {
 		em.persist(commande);
 	}
 
-	public Commande getRecapById(Long idCommande) {
+	public Commande getCommandeById(Long idCommande) {
 		return em.find(Commande.class, idCommande);
 	}
 
-	public Commande getCommandeSuivante() {
-		List<Commande> commandes =  em.createQuery("select c from Commande c where c.enAttente = true order by c.dateEmission", Commande.class)
-									  .setMaxResults(1)
-									  .getResultList();
-		return commandes.get(0);
-	}
-
-	public List<Commande> getCommandesEnAttente() {
-		return em.createQuery("select c from Commande c where c.enAttente = true order by c.dateEmission", Commande.class)
-				 .getResultList();
-	}
-
-	public void signalerCommandePrete(Long id) {
-		em.createQuery("update Commande c set c.enAttente = false where c.id = :id")
-		  .setParameter("id", id)
-		  .executeUpdate();
+	public List<Commande> getCommandes() {
+		return em.createQuery("select * from Commande", Commande.class).getResultList();
 	}
 
 }
