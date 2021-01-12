be20347d8bd1e2e1eb4198b1d991f1ebc798f5b4
bacary <bacaryd33@gmail.com>
2021-01-12 22:52:36 +0100

    entreprise ok

--- src/main/java/auto/spring/dao/EntrepriseDAO.java
+++ src/main/java/auto/spring/dao/EntrepriseDAO.java
@@ -1,1 +1,30 @@
 package auto.spring.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import auto.spring.modele.Entreprise;
+
+@Repository
+public class EntrepriseDAO {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	public void sauver(Entreprise entreprise) {
+		em.persist(entreprise);
+	}
+
+	public Entreprise getentrepriseById(Long idEntreprise) {
+		return em.find(Entreprise.class, idEntreprise);
+	}
+
+	public List<Entreprise> getentreprises() {
+		return em.createQuery("select * from entreprise", Entreprise.class).getResultList();
+	}
+
+}
